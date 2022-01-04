fa44ecc146d20f973ec279947bcacc9e93f9d354
Bekirov Timur <bekirovtimur@gmail.com>
2022-01-04 15:41:45 +0300

    0

--- Script/002_deploy_VM.ps1
+++ Script/002_deploy_VM.ps1
@@ -1,6 +1,2 @@
-New-AzResourceGroupDeployment `
-  -Name vm-timurbekirov-deployment `
-  -ResourceGroupName timurbekirov-labrg `
-  -TemplateUri https://raw.githubusercontent.com/bekirovtimur/azure-course/main/ARM/template.json `
-  -TemplateParameterUri https://raw.githubusercontent.com/bekirovtimur/azure-course/main/ARM/parameters.json
+New-AzResourceGroupDeployment -Name vm-timurbekirov-deployment -ResourceGroupName timurbekirov-labrg -TemplateUri https://raw.githubusercontent.com/bekirovtimur/azure-course/main/ARM/template.json -TemplateParameterUri https://raw.githubusercontent.com/bekirovtimur/azure-course/main/ARM/parameters.json
 
