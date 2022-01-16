4fdae53357cdedd7d7e4db3216430fe2d94b917b
Bekirov Timur <bekirovtimur@gmail.com>
2022-01-16 16:02:09 +0300

    Another one try =)

--- Script/02_deploy_VM.ps1
+++ Script/02_deploy_VM.ps1
@@ -2,5 +2,8 @@ $resourceGroupName = "timurbekirov-lab-rg"
 $templateUri = "https://raw.githubusercontent.com/bekirovtimur/azure-course/main/ARM/VM/template.json"
 $templateParameterUri = "https://raw.githubusercontent.com/bekirovtimur/azure-course/main/ARM/VM/parameters.json"
 
-#Test-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -TemplateParameterUri $templateParameterUri
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -TemplateParameterUri $templateParameterUri
+#Test-AzResourceGroupDeployment `
+New-AzResourceGroupDeployment `
+  -ResourceGroupName $resourceGroupName `
+  -TemplateUri $templateUri `
+  -TemplateParameterUri $templateParameterUri
